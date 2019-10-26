24b8e41dad243a534ca4b78724e73f1a5651c4af
arquinter <alexanderquinter@yahoo.com>
2019-10-26 17:45:47 -0400

    project 2
    update

--- project_2/app.R
+++ project_2/app.R
@@ -6,7 +6,47 @@
 #
 #    http://shiny.rstudio.com/
 #
+
 source("C:/Users/alex/Documents/GitHub/bios611-projects-fall-2019-arquinter/project_2/helper_functions.R")
+
+ui <- navbarPage("Navbar",
+                 tabPanel("Plot",
+                          sidebarLayout(
+                            sidebarPanel(
+                              radioButtons("org", "Organization",
+                                           c("Total Homeless"="Total Homeless", "Chronically Homeless"="Chronically Homeless",
+                                             "Sheltered Homeless" = "Sheltered Homeless", "Sheltered Chronically Homeless" = 
+                                               "Sheltered Chronically Homeless", "Unsheltered Homeless" = "Unsheltered Homeless",
+                                             "Unsheltered Chronically Homeless" = "Unsheltered Chronically Homeless")
+                              )
+                            ),
+                            mainPanel(
+                              plotOutput("countplot")
+                            )
+                          )
+                 )
+)
+
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$countplot <- renderPlot({
+    
+    data = switch(input$org,
+                  "Total Homeless" = Durham.homeless.counts[Durham.homeless.counts[,5]=="Total Homeless",],
+                  "Chronically Homeless" = Durham.homeless.counts[Durham.homeless.counts[,5]=="Chronically Homeless",],
+                  "Sheltered Homeless" = Durham.homeless.counts[Durham.homeless.counts[,5]=="Sheltered Homeless",],
+                  "Sheltered Chronically Homeless" = Durham.homeless.counts[Durham.homeless.counts[,5]=="Sheltered Chronically Homeless",],
+                  "Unsheltered Homeless" = Durham.homeless.counts[Durham.homeless.counts[,5]=="Unsheltered Homeless",],
+                  "Unsheltered Chronically Homeless" = Durham.homeless.counts[Durham.homeless.counts[,5]=="Unsheltered Chronically Homeless",])
+    
+    ggplot(data, aes(x=year, y=count_)) + geom_bar(stat = "identity")
+    
+  })
+}
 
 shinyApp(ui = ui, server = server)
 
+deployApp()
